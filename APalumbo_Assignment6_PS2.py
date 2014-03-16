95fc75d79a7c5c21b2864263037999b049d4f553
ajp619 <ajp619@outlook.com>
2014-03-16 15:32:14 -0700

    Added code to summarize combinations of user input, as requested in assignment. Added word_combo helper function. Modified formatting to make it easier to compare results.

--- APalumbo_Assignment6_PS2.py
+++ APalumbo_Assignment6_PS2.py
@@ -34,6 +34,11 @@ def clean_text(mystring):
     return mystring.lower()
 
 
+def word_combos(word_list, combo_length):
+    # Helper function to construct the ngrams
+    return [' '.join(word_list[i:i+combo_length]) for i in range(len(word_list)) if i+combo_length <= len(word_list)]
+
+
 def n_word_count(mytext, upto=1):
     # Break the text up into words. Expects clean text.
     words = mytext.split()
@@ -44,7 +49,7 @@ def n_word_count(mytext, upto=1):
     #           total_words is a count of all word combos
     summary = {}
     for n in range(1, upto+1):
-        add_to_list = [' '.join(words[i:i+n]) for i in range(0, len(words)) if i+n <= len(words)]
+        add_to_list = word_combos(words, n)
         summary[n] = dict(word_counts={w:f for w,f in Counter(add_to_list).most_common()},
                           total_words=len(add_to_list))
     return summary
@@ -54,45 +59,61 @@ def get_input(n):
     # Get phrase from user. Return to quit. Limit to n words.
     # Clean user input so that it is lowercase and has no punctuation
     while True:
-        input_phrase = raw_input('Enter up to a {0} word combination\nReturn to quit:'.format(n))
+        input_phrase = raw_input('\nEnter up to a {0} word combination\nReturn to quit:'.format(n))
         input_list = clean_text(input_phrase.decode('utf-8')).split()
         if 0 <= len(input_list) <= n:
             return ' '.join(input_list)
         print 'Too many words'
 
 
-def ngram_summary(summary, n):
-    # Get input. Calculate probability based on appropriate ngram summary.
+def ngram_summary(input_value, summary):
+    try:
+        ngram = len(input_value.split())
+        # summary[ngram] is a dict with key 'word_counts' which is a dict. lookup input_value here.
+        occur = summary[ngram]['word_counts'][input_value]
+        total_words = summary[ngram]['total_words']
+        print "'{0}' \noccurred {2} times out of {3} total combinations of {4} words " \
+              "for a probability of {1:.4f}".format(input_value,
+                                                    float(occur)/float(total_words),
+                                                    occur,
+                                                    total_words,
+                                                    ngram)
+    except KeyError:
+        if len(input_value) != 0:
+            print '{0} does not exist in the text\n'.format(input_value)
+
+
+def process_input(summary, n):
+    # Process user input. Quit if no input.
     input_value = ' '
     while len(input_value) != 0:
+        # Get input
         input_value = get_input(n)
-        try:
-            ngram = len(input_value.split())
-            # summary[ngram] is a dict with key 'word_counts' which is a dict. lookup input_value here.
-            occur = summary[ngram]['word_counts'][input_value]
-            total_words = summary[ngram]['total_words']
-            print "Based on the input text, '{0}' occurred {2} times \n" \
-                  "out of {3} total combinations of {4} words \n" \
-                  "so the probability of '{0}' occurring is {1:.4f}\n".format(input_value,
-                                                                              float(occur)/float(total_words),
-                                                                              occur,
-                                                                              total_words,
-                                                                              ngram)
-        except KeyError:
-            if len(input_value) != 0:
-                print '{0} does not exist in the text\n'.format(input_value)
+
+        # Split input into words
+        input_list = input_value.split()
+
+        # Generate interrogation list
+        entry_list = input_list[:]
+        nwords = len(input_list)
+        for i in range(2, nwords+1):
+            entry_list = entry_list + word_combos(input_list, i)
+
+        # Summarize for each entry
+        for entry in entry_list:
+            ngram_summary(entry, summary)
 
 
 def main():
     file_name = "assign6.sample.txt"        # Expect file in same directory
     data = read_text_file(file_name)
     data = clean_text(data)
-    n = 3
+    n = 4
     # Create summary
     wordcount = n_word_count(data, n)
 
     # Let user interrogate summary
-    ngram_summary(wordcount, n)
+    process_input(wordcount, n)
 
 
 if __name__ == "__main__":
